6b057c66b45a87b9cadfd6e16d09d67deca20f16
Matt Thomas <matt@betweenbrain.com>
2014-04-09 14:18:31 -0400

    Adds joined query to get k2 item data and locations data that belong to designated additional category

--- site/models/mapnavigator.php
+++ site/models/mapnavigator.php
@@ -36,14 +36,23 @@ class MapnavigatorModelMapnavigator extends JModel
 	}
 
 	/**
-	 * Gets the greeting
+	 * Get the K2 item and locations data that correspond to items in the additional categories as designated by the categories URL parameter
 	 *
 	 * @return string The greeting to be displayed to the user
 	 */
 	function getItems()
 	{
-		$query = ' SELECT locations' .
-			' FROM #__k2_items_locations';
+		$query = ' SELECT ' .
+			$this->db->nameQuote('k2.title') . ',' .
+			$this->db->nameQuote('k2.introtext') . ',' .
+			$this->db->nameQuote('loc.locations') .
+			' FROM ' . $this->db->nameQuote('#__k2_items') . ' AS ' . $this->db->nameQuote('k2') .
+			' JOIN ' . $this->db->nameQuote('#__k2_items_locations') . ' AS ' . $this->db->nameQuote('loc') .
+			' ON (' . $this->db->nameQuote('loc.itemId') . ' = ' . $this->db->nameQuote('k2.id') . ')' .
+			' JOIN ' . $this->db->nameQuote('#__k2_additional_categories') . ' AS ' . $this->db->nameQuote('cats') .
+			' ON (' . $this->db->nameQuote('k2.id') . ' = ' . $this->db->nameQuote('cats.itemId') . ')' .
+			' WHERE ' . $this->db->nameQuote('cats.catid') . ' IN (' . implode(',', JRequest::getVar('categories')) . ')' .
+			' AND ' . $this->db->nameQuote('k2.trash') . ' = ' . $this->db->quote('0');
 
 		$this->db->setQuery($query);
 
