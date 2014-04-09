91ce3511a5d628443046613868a37a8956a1bc3b
Matt Thomas <matt@betweenbrain.com>
2014-04-09 11:35:16 -0400

    Updates docblock

--- site/controller.php
+++ site/controller.php
@@ -2,31 +2,33 @@
 
 /**
  * File       controller.php
- * Created    4/9/14 11:08 AM 
+ * Created    4/9/14 11:08 AM
  * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
  * Support    https://github.com/betweenbrain/
  * Copyright  Copyright (C) 2014 betweenbrain llc. All Rights Reserved.
  * License    GNU GPL v2 or later
  */
- 
+
 jimport('joomla.application.component.controller');
 
 /**
- * Hello World Component Controller
+ * MAP Navigator Component Controller
  *
- * @package    Joomla.Tutorials
- * @subpackage Components
+ * @package     Joomla.Tutorials
+ * @subpackage  Components
+ *
+ * @since       0.1
  */
 class MapnavigatorController extends JController
 {
-    /**
-     * Method to display the view
-     *
-     * @access    public
-     */
-    function display()
-    {
-        parent::display();
-    }
+	/**
+	 * Method to display the view
+	 *
+	 * @access    public
+	 */
+	function display()
+	{
+		parent::display();
+	}
 
 }
