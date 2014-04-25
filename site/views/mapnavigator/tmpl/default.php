a2d4e0bb766878ce19bfd14a60edcba54057c682
Matt Thomas <matt@betweenbrain.com>
2014-04-25 11:06:04 -0400

    Split category selection lists in half

--- site/views/mapnavigator/tmpl/default.php
+++ site/views/mapnavigator/tmpl/default.php
@@ -9,26 +9,31 @@
  * License    GNU GPL v2 or later
  */
 $params = & JComponentHelper::getParams('com_mapnavigator');
+$half = floor(count($this->categories) / 2);
 ?>
 <section class="map-navigator">
 	<div class="panel">
 		<ul id="sidebar"></ul>
 		<form id="filters">
 			<ul>
-				<?php foreach ($this->categories as $category) : ?>
-					<li>
-						<label>
-							<input class="filters" type="checkbox" name="categories[]" value="<?php echo $category->id ?>"><?php echo $category->name ?>
-						</label>
-						<?php if ($category->id === $params->get('artistCategory')) : ?>
-							<div class="toggle-btn-group clearfix">
-								<input id="birth" type="radio" name="location" value="birth">
-								<label for="birth">Born</label>
-								<input id="primary" type="radio" name="location" value="primary" checked="checked">
-								<label for="primary">Works</label>
-							</div>
-						<?php endif ?>
-					</li>
+				<?php foreach ($this->categories as $index => $category) : ?>
+				<?php if ($index == $half) : ?>
+			</ul>
+			<ul>
+				<?php endif ?>
+				<li>
+					<label>
+						<input class="filters" type="checkbox" name="categories[]" value="<?php echo $category->id ?>"><?php echo $category->name ?>
+					</label>
+					<?php if ($category->id === $params->get('artistCategory')) : ?>
+						<div class="toggle-btn-group clearfix">
+							<input id="birth" type="radio" name="location" value="birth">
+							<label for="birth">Born</label>
+							<input id="primary" type="radio" name="location" value="primary" checked="checked">
+							<label for="primary">Works</label>
+						</div>
+					<?php endif ?>
+				</li>
 				<?php endforeach ?>
 			</ul>
 		</form>
