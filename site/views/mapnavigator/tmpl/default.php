c58331cd07f84c10a2cc8e697c433da3ed8d5e93
Matt Thomas <matt@betweenbrain.com>
2014-04-25 06:13:04 -0400

    Add tooltips

--- site/views/mapnavigator/tmpl/default.php
+++ site/views/mapnavigator/tmpl/default.php
@@ -11,32 +11,34 @@
 $params = & JComponentHelper::getParams('com_mapnavigator');
 ?>
 <section class="map-navigator">
-	<ul id="sidebar"></ul>
-	<form id="filters">
-		<ul>
-			<?php foreach ($this->categories as $category) : ?>
-				<li>
-					<label>
-						<input class="filters" type="checkbox" name="categories[]" value="<?php echo $category->id ?>"><?php echo $category->name ?>
-					</label>
-					<?php if ($category->id === $params->get('artistCategory')) : ?>
-						<div class="toggle-btn-group clearfix">
-							<input id="birth" type="radio" name="location" value="birth">
-							<label for="birth">Born</label>
-							<input id="primary" type="radio" name="location" value="primary" checked="checked">
-							<label for="primary">Works</label>
-						</div>
-					<?php endif ?>
-				</li>
-			<?php endforeach ?>
-		</ul>
-	</form>
+	<div class="panel">
+		<ul id="sidebar"></ul>
+		<form id="filters">
+			<ul>
+				<?php foreach ($this->categories as $category) : ?>
+					<li>
+						<label>
+							<input class="filters" type="checkbox" name="categories[]" value="<?php echo $category->id ?>"><?php echo $category->name ?>
+						</label>
+						<?php if ($category->id === $params->get('artistCategory')) : ?>
+							<div class="toggle-btn-group clearfix">
+								<input id="birth" type="radio" name="location" value="birth">
+								<label for="birth">Born</label>
+								<input id="primary" type="radio" name="location" value="primary" checked="checked">
+								<label for="primary">Works</label>
+							</div>
+						<?php endif ?>
+					</li>
+				<?php endforeach ?>
+			</ul>
+		</form>
+	</div>
 	<form id="toolbar">
-		<label class="global">
+		<label class="global" title="All Regions">
 			<input type="radio" name="region" value="">Global
 		</label>
 		<?php foreach ($this->regions as $region) : ?>
-			<label class="<?php echo $region->alias ?>">
+			<label class="<?php echo $region->alias ?>" title="<?php echo $region->name ?>">
 				<input type="radio" name="region" value="<?php echo $region->id ?>"><?php echo $region->name ?>
 			</label>
 		<?php endforeach ?>
